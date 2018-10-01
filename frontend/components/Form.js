40fbfab1fdc430265d69f08cd1bc11549b4cc3c9
David Anaya <davidanayadesign@gmail.com>
2018-10-01 16:19:18 -0700

    Form functionality

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import '@zendeskgarden/react-buttons/dist/styles.css';
+import '@zendeskgarden/react-textfields/dist/styles.css';
 
-class Form extends Component {
+import { Button } from '@zendeskgarden/react-buttons';
+import { TextField, Textarea, Label, Input } from '@zendeskgarden/react-textfields';
+
+class Form extends Component { 
+
   render() {
     return (
       <form className="blog--post--form">
-        <input placeholder="Post heading" />
-        <textarea />
-        <button>Submit</button>
+        <TextField>
+          <Label>Post Heading
+          <Input placeholder="Input your post heading" />
+          </Label>
+        </TextField>
+        <Label>Post content
+          <Textarea placeholder="Write your post"/>
+        </Label>
+        <Button onClick={()=>this.props.addBlogItem}>Submit</Button>
       </form>
     );
   }
