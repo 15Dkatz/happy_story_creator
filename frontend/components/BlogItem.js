f5f31401bbfc67505487c0ae1bd14ff851baca24
David Anaya <davidanayadesign@gmail.com>
2018-10-01 16:17:11 -0700

    Blog Item component

--- frontend/components/BlogItem.js
+++ frontend/components/BlogItem.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import { Anchor } from "@zendeskgarden/react-buttons";
+
 class Form extends Component {
   render() {
     return (
       <div>
         <div>
-          <h2>{this.props.title}</h2>
+          <h3>{this.props.title}</h3>
           <p>{this.props.content}</p>
         </div>
         <div className="blog--post--below">
           <div className="blog--post--share">
-            <a href="">Facebook</a>
-            <br />
-            <a href="">Twitter</a>
-            <br />
-            <a href="">Instagram</a>
+          
+          <p><small><Anchor href="#">Facebook</Anchor> | <Anchor href="#">Twitter</Anchor> | <Anchor href="#">LinkedIn</Anchor></small></p>
           </div>
         </div>
       </div>
