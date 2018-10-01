90c45ba5e93ee553b53fa96da147967801a7ae63
David Anaya <davidanayadesign@gmail.com>
2018-10-01 13:08:09 -0700

    Blogging components

--- frontend/components/Blogging.js
+++ frontend/components/Blogging.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Form from "./Form";
+import BlogItem from "./BlogItem";
+import "./blogging.css";
 
 class Blogging extends Component {
   render() {
     return (
       <div>
         <h2>Blogging</h2>
-        <Link to='/'>Home</Link>
+        <Link to="/">Home</Link>
+        <div className="blog--display">
+          <BlogItem title="Post heading" content="Post content" />
+        </div>
+        <hr />
+        <main className="blog--content">
+          <div className="blog--post">
+            <Form />
+          </div>
+          <aside className="sidebar">
+            <div className="blog--post--valueprop">
+              <p>Advertisement</p>
+            </div>
+            <p>thermometer</p>
+          </aside>
+        </main>
       </div>
     );
   }
